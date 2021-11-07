5049d9b5379c591d8790e605ad8144acd5ab6f23
TassioSales <tassio.ljs@gmail.com>
2021-11-06 23:28:21 -0300

    Adicionando exercicios 5 e 6

---
+++ 1-EstruturaSequencial/exercicio_5.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Digite o tamanho em metros que deseja converte: "))
+
+centimetros = metros * 100
+
+print(f"O tamanho {metros} m² convertido para milimetros e  {centimetros}cm")
---
+++ 1-EstruturaSequencial/exercicio_6.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = float(input("Digite o raio do circulo: "))
+
+area = 3.14 * (raio ** 2) 
+
+print(f"A area do circulo e {area}")
+
