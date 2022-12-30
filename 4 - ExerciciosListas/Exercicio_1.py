c2846d7e29a47fb557b7f6077ea3d02652d1063f
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-12-30 18:49:57 -0300

    Create Exercicio_1.py

---
+++ 4 - ExerciciosListas/Exercicio_1.py
@@ -0,0 +1,9 @@
+"""Faça um Programa que leia um vetor de 5 números inteiros e mostre-os."""
+
+def lervetor(vetor):
+    for item in vetor:
+        print(item)
+        
+vetor = [1,2,3,4,5]
+
+lervetor(vetor)
