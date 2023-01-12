787be1fb0053846bfaaa407821f72cccaedb1b12
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-12 14:45:32 -0300

    Create teste.py

---
+++ 4 - ExerciciosListas/teste.py
@@ -0,0 +1,9 @@
+#criar um lambda que recebe um vetor e imprime os itens do vetor
+
+vetor = [1,2,3,4,5]
+
+imprimir = lambda vetor: print(vetor for item in vetor)
+
+# mostrar o resultado do lambda
+
+print(imprimir(vetor))
