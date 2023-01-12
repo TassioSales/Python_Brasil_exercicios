00a7e5ffceaa29461ed0d33084fb7ac4aea45692
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-12 14:56:47 -0300

    Update Exercicio_22.py

--- 4 - ExerciciosListas/Exercicio_22.py
+++ 4 - ExerciciosListas/Exercicio_22.py
@@ -12,4 +12,54 @@ Situação                        Quantidade              Percentual
 4- quebrado ou inutilizado              15                     15%
 """
 
-class 
+class Meta:
+    def __init__(self):
+        self.identificacao = 0
+        self.defeito = 0
+        self.lista = []
+        self.esfera = 0
+        self.limpeza = 0
+        self.cabo = 0
+        self.quebrado = 0
+    
+    def menu(self):
+        print('''
+        1- necessita da esfera
+        2- necessita de limpeza
+        3- necessita troca do cabo ou conector
+        4- quebrado ou inutilizado
+        0- Sair
+        ''')
+
+    def calcula(self):
+        while True:
+            self.identificacao = int(input('Digite a identificação do mouse: '))
+            if self.identificacao == 0:
+                break
+            else:
+                self.menu()
+                self.defeito = int(input('Digite o defeito: '))
+                if self.defeito == 0:
+                    break
+                elif self.defeito > 4:
+                    print('Opção inválida!')
+                else:
+                    self.lista.append(self.defeito)
+        self.esfera = self.lista.count(1)
+        self.limpeza = self.lista.count(2)
+        self.cabo = self.lista.count(3)
+        self.quebrado = self.lista.count(4)
+        print(f'''
+        Quantidade de mouses: {len(self.lista)}
+        
+        Situação                        Quantidade              Percentual
+        1- necessita da esfera                  {self.esfera}                     {self.esfera / len(self.lista) * 100}%
+        2- necessita de limpeza                 {self.limpeza}                     {self.limpeza / len(self.lista) * 100}%
+        3- necessita troca do cabo ou conector  {self.cabo}                     {self.cabo / len(self.lista) * 100}%
+        4- quebrado ou inutilizado              {self.quebrado}                     {self.quebrado / len(self.lista) * 100}%
+        ''')
+        
+
+if __name__ == '__main__':
+    meta = Meta()
+    meta.calcula()
