4525315a26af5dfa414a832183e55c7f24c39f92
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-23 22:23:31 -0300

    Update Exercicio_3.py

--- 5 - ExerciciosFuncoes/Exercicio_3.py
+++ 5 - ExerciciosFuncoes/Exercicio_3.py
@@ -2,6 +2,7 @@
 Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
 """
 
+
 def pedevalor():
     lista = []
     for i in range(3):
@@ -9,16 +10,19 @@ def pedevalor():
         lista.append(n)
     return lista
 
+
 def somaLista(lista):
     soma = 0
     for i in range(len(lista)):
         soma += lista[i]
     return soma
 
+
 def main():
     lista = pedevalor()
     soma = somaLista(lista)
     print(soma)
-    
+
+
 if __name__ == "__main__":
     main()
