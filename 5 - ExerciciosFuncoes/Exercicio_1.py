cfecba162f5afd8af75816097de42a1425986d97
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-23 22:23:26 -0300

    Update Exercicio_1.py

--- 5 - ExerciciosFuncoes/Exercicio_1.py
+++ 5 - ExerciciosFuncoes/Exercicio_1.py
@@ -8,19 +8,21 @@ Faça um programa para imprimir:
 para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
 """
 
+
 def pedevalor():
     n = int(input("Digite um número: "))
-    return n        
+    return n
+
 
 def imprime(n):
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         print(i * str(i))
-        
+
+
 def main():
     n = pedevalor()
     imprime(n)
-    
-    
+
+
 if __name__ == "__main__":
     main()
-
