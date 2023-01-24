cf4f6c03bb6db4eebd33e89e961e28d038376f4e
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-23 22:23:34 -0300

    Update Exercicio_4.py

--- 5 - ExerciciosFuncoes/Exercicio_4.py
+++ 5 - ExerciciosFuncoes/Exercicio_4.py
@@ -2,20 +2,24 @@
 Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere ‘P’, se seu argumento for positivo, e ‘N’, se seu argumento for zero ou negativo.
 """
 
+
 def pedenumero():
     n = int(input("Digite um número: "))
     return n
+
 
 def positivoOuNegativo(n):
     if n > 0:
         return "P"
     else:
         return "N"
-    
+
+
 def main():
     n = pedenumero()
     resultado = positivoOuNegativo(n)
     print(resultado)
-    
+
+
 if __name__ == "__main__":
     main()
