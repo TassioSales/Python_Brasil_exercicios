852fc98b8e1a6e13b3b603f0c8622cecb1b7e2be
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-23 22:23:36 -0300

    Update Exercicio_5.py

--- 5 - ExerciciosFuncoes/Exercicio_5.py
+++ 5 - ExerciciosFuncoes/Exercicio_5.py
@@ -2,10 +2,12 @@
 Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas.
 """
 
+
 def pedevalor():
     taxaDeimposto = float(input("Digite a taxa de imposto: "))
     custo = float(input("Digite o custo: "))
     return taxaDeimposto, custo
+
 
 def calculaImposto(taxaDeimposto, custo):
     imposto = (taxaDeimposto / 100) * custo
@@ -17,6 +19,7 @@ def main():
     taxaDeimposto, custo = pedevalor()
     custo = calculaImposto(taxaDeimposto, custo)
     print(custo)
-    
+
+
 if __name__ == "__main__":
-    main()  
+    main()
