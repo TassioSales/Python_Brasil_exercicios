086c20323d09b08dba0688abc072bccdb0dc850b
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-23 22:23:44 -0300

    Update Exercicio_8.py

--- 5 - ExerciciosFuncoes/Exercicio_8.py
+++ 5 - ExerciciosFuncoes/Exercicio_8.py
@@ -1,8 +1,10 @@
 "Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado."
 
+
 def pedeNumero():
     numero = int(input("Digite um número inteiro: "))
     return numero
+
 
 def contaDigito(numero):
     cont = 0
@@ -10,11 +12,13 @@ def contaDigito(numero):
     for num in numero:
         cont += 1
     return cont
- 
+
+
 def main():
     numero = pedeNumero()
     cont = contaDigito(numero)
     print("O número %d tem %d dígitos." % (numero, cont))
-    
+
+
 if __name__ == "__main__":
     main()
