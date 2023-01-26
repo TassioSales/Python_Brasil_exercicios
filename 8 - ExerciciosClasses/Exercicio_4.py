93e11ea1f417831ed3a554b784556a018bb13c5f
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-26 10:54:37 -0300

    Update Exercicio_4.py

--- 8 - ExerciciosClasses/Exercicio_4.py
+++ 8 - ExerciciosClasses/Exercicio_4.py
@@ -3,6 +3,7 @@
 Atributos: nome, idade, peso e altura Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão,
 a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm."""
 
+
 class Pessoa:
     def __init__(self, nome, idade, peso, altura):
         self.nome = nome
