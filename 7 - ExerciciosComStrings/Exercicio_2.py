59243b17307c3dfef074074081e86c7c45cc01f8
TassioSales <74218122+TassioSales@users.noreply.github.com>
2023-01-26 09:47:02 -0300

    Create Exercicio_2.py

---
+++ 7 - ExerciciosComStrings/Exercicio_2.py
@@ -0,0 +1,67 @@
+"""A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos.
+Para tentar resolver este problema, o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários, e
+identificar os usuários com maior espaço ocupado. Através de um programa, baixado da Internet, ele conseguiu gerar
+o seguinte arquivo, chamado "usuarios.txt":
+
+alexandre       456123789
+anderson        1245698456
+antonio         123456456
+carlos          91257581
+cesar           987458
+rosemary        789456125
+
+Neste arquivo, o nome do usuário possui 15 caracteres. A partir deste arquivo, você deve criar um programa que gere um
+relatório, chamado "relatório.txt", no seguinte formato:
+ACME Inc.               Uso do espaço em disco pelos usuários
+------------------------------------------------------------------------
+Nr.  Usuário        Espaço utilizado     % do uso
+
+1    alexandre       434,99 MB             16,85%
+2    anderson       1187,99 MB             46,02%
+3    antonio         117,73 MB              4,56%
+4    carlos           87,03 MB              3,37%
+5    cesar             0,94 MB              0,04%
+6    rosemary        752,88 MB             29,16
+
+Espaço total ocupado: 2581,57 MB
+Espaço médio ocupado: 430,26 MB
+
+O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em memória, caso sejam necessários, de forma a
+agilizar a execução do programa. A conversão da espaço ocupado em disco, de bytes para megabytes deverá ser feita
+através de uma função separada, que será chamada pelo programa principal. O cálculo do percentual de uso também deverá
+ser feito através de uma função, que será chamada pelo programa principal."""
+
+
+
+def converte_bytes_para_megabytes(bytes):
+    return bytes / 1024 / 1024
+
+
+def calcula_percentual_de_uso(bytes, total):
+    return (bytes / total) * 100
+
+
+def main():
+    total = 0
+    usuarios = []
+    with open('usuarios.txt', 'r') as arquivo:
+        for linha in arquivo:
+            usuario, bytes = linha.split()
+            bytes = int(bytes)
+            total += bytes
+            usuarios.append((usuario, bytes))
+
+    with open('relatorio.txt', 'w', encoding='utf-8') as arquivo:
+        arquivo.write('ACME Inc.               Uso do espaço em disco pelos usuários\n')
+        arquivo.write('------------------------------------------------------------------------\n')
+        arquivo.write('Nr.  Usuário        Espaço utilizado     % do uso\n\n')
+
+        for i, (usuario, bytes) in enumerate(usuarios):
+            arquivo.write(f'{i + 1:<4} {usuario:<15} {converte_bytes_para_megabytes(bytes):>10.2f} MB '
+                          f'{calcula_percentual_de_uso(bytes, total):>10.2f}%\n')
+
+        arquivo.write(f'\nEspaço total ocupado: {converte_bytes_para_megabytes(total):.2f} MB\n')
+        arquivo.write(f'Espaço médio ocupado: {converte_bytes_para_megabytes(total / len(usuarios)):.2f} MB\n')
+
+if __name__ == '__main__':
+    main()
